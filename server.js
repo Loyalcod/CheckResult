afa7820bb0a1bb1c85e8bf9b1d694d39271f5bce
akujuobi victor <victorakujuobichibuike@gmail.com>
2022-10-20 11:18:10 +0100

    new changes

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const server = express()
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const path = require("path")
+const connectDB = require("./config/db")
+require("dotenv").config({path: path.resolve(__dirname,'./.env')})
+
+connectDB()
+const port = process.env.PORT
+server.use(bodyParser.urlencoded({extended: false}))
+server.use(bodyParser.json())
+server.use(cookieParser())
+
+
+server.get('/',(req,res)=>{
+    res.send("this is the server side")
+})
+
+
+
+server.listen(port,()=>{
+    console.log("this server is running fine")
+})
